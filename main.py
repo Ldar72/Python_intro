055588f7eb483314e5f7a9d392c8026907fa8ff9
Ldar72 <l-dar@mail.ru>
2024-07-29 04:45:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# 1st program.
+print(9**0.5*5)
+# 2nd program.
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+a = 1234
+b = 5678
+print(a % 1000 // 10 + b % 5000 // 10)
+# 4th program
+a = '13.42'
+b = '42.13'
+print(int(a[0] + a[1]) == (int(b[3] + b[4]))) and (int(a[3] + a[4]) == (int(b[0] + b[1])))
+
